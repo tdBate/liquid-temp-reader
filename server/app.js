a5211246e25f16c2802334345417aeec89e4c2df
Torok Donat <td.zazsa@gmail.com>
2026-02-10 23:35:46 +0100

    node server and file structure

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+app.use(express.static(path.join(__dirname, "..","client", "dist")));
+
+app.get('/index',(req,res)=>{
+    res.sendFile(path.join(__dirname,"..","client","dist","index.html"));
+})
+
+app.listen(3000, () => {
+    console.log(__dirname);
+});
